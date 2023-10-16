3063de0daf0988b0955f51e3bb585734404c914c
Raj Mandloii <rajmandloi01.01@gmail.com>
2023-10-16 14:11:54 +0530

    minor changes

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,22 +3,51 @@ import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function SignupPage() {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
     username: "",
   });
 
-  const onSignup = async () => {};
+  const [buttonDisabled, setDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+      console.log(response.data);
+      router.push("/login");
+    } catch (e: any) {
+      toast.error(e.message);
+      console.log(e.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  React.useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [user]);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Signup</h1>
+      <h1>{loading ? "Proccessing ..........." : "Signup"}</h1>
       <hr />
       <label htmlFor="username">username</label>
       <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
         type="text"
         id="usename"
         value={user.username}
@@ -27,7 +56,7 @@ export default function SignupPage() {
       />
       <label htmlFor="email">email</label>
       <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="p-2 border text-black border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
         type="text"
         id="usename"
         value={user.email}
@@ -36,7 +65,7 @@ export default function SignupPage() {
       />
       <label htmlFor="password">password</label>
       <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
         type="password"
         id="usename"
         value={user.password}
@@ -47,9 +76,9 @@ export default function SignupPage() {
         onClick={onSignup}
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
       >
-        Sign Up
+        {buttonDisabled ? "No signup" : "Sign Up"}
       </button>
-      <Link href={'/login'}>Visit Login Page</Link>
+      <Link href={"/login"}>Visit Login Page</Link>
     </div>
   );
 }
